d22391c3e9a800bd8c475011fae1a29bfae0bb82
lieby weintraub <liebahinda@gmail.com>
2019-09-06 03:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  return drivers.filter(function (drivers) driver)
+  return drivers.filter(function (drivers) { drivers.to Lowercase())
   
 };
