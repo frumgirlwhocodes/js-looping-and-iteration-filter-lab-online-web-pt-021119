937dfb3ed76ec056eee4bed1b6af67800d1f7cb7
lieby weintraub <liebahinda@gmail.com>
2019-09-06 03:41:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  return drivers.filter(function (drivers) { return drivers.toLowerCase()=== name.toLowerCase)
+  return drivers.filter(function (drivers) { return drivers.toLowerCase()=== name.toLowerCase()})
   
 };
